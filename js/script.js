8965e146619f29cf2d561ee97dbbda0de3234984
AnthonyBussi <61181969+AnthonyBussi@users.noreply.github.com>
2020-12-14 10:43:17 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.js-scrollTo').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
-        var speed = 250; // Durée de l'animation (en ms)
+        var speed = 450; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
         return false;
     });
