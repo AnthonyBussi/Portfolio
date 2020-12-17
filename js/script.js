757200525a6efb39b4e6d1406a79dc04f0afdc95
AnthonyBussi <61181969+AnthonyBussi@users.noreply.github.com>
2020-12-17 09:19:46 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
+function horizontalScroll(){
+    var hauteur = $(document).height()-$(window).height();
+    var position = $(document).scrollTop();
+    var largeur = $(window).width();	
+    var barre = position / hauteur * largeur;	
+    $("#scroll-line").css("width", barre);
+}
+
 function scrollToLink(){
     var page = $(this).attr('href');
     var speed = 700;
     $('html, body').animate({scrollTop : $(page).offset().top}, speed);
     return false;
 }
-
 
 function menuHamburger()
 {
@@ -14,6 +21,7 @@ function menuHamburger()
 // Gestionnaire d'évènements
 $(function()
     {
+	$(document).on('scroll', horizontalScroll);
         $('.navbar-toggle').on("click",menuHamburger);
         $('.js-scrollTo').on('click', scrollToLink);
     }
